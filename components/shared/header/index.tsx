a6a2ae954666c99a332b8d657e284c50e1e2ebd1
Leonardoesc <163489848+Sushiland123@users.noreply.github.com>
2025-07-27 18:23:17 -0600

    Initial commit - Prostore ecommerce

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,33 +1,22 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { APP_NAME } from '@/lib/constants';
-import Menu from './menu';
-import CategoryDrawer from './category-drawer';
-import Search from './search';
 
 const Header = () => {
   return (
     <header className='w-full border-b'>
-      <div className='wrapper flex-between'>
-        <div className='flex-start'>
-          <CategoryDrawer />
-          <Link href='/' className='flex-start ml-4'>
-            <Image
-              src='/images/logo.svg'
-              alt={`${APP_NAME} logo`}
-              height={48}
-              width={48}
-              priority={true}
-            />
-            <span className='hidden lg:block font-bold text-2xl ml-3'>
+      <div className='max-w-7xl mx-auto p-5 flex justify-between items-center'>
+        <div className='flex items-center'>
+          <Link href='/' className='flex items-center'>
+            <span className='font-bold text-2xl'>
               {APP_NAME}
             </span>
           </Link>
         </div>
-        <div className='hidden md:block'>
-          <Search />
+        <div>
+          <Link href='/cart' className='px-4 py-2 border rounded'>
+            Cart
+          </Link>
         </div>
-        <Menu />
       </div>
     </header>
   );
